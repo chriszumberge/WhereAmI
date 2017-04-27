040bfb6c155d1bd1c131694923cb51558b693f54
Chris Zumberge <chriszumberge@gmail.com>
2017-04-27 13:43:19 -0400

    Added some testing tools

---
+++ WhereAmI/WhereAmI.Debug/Program.cs
@@ -0,0 +1,157 @@
+﻿using Plugin.Geolocator.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using WhereAmI.Mock;
+
+namespace WhereAmI.Debug
+{
+    class Program
+    {
+        static GeofenceManager manager { get; set; }
+
+        static void Main(string[] args)
+        {
+            ApplicationState debuggingAppState = new ApplicationState();
+
+            manager = new GeofenceManager();
+            WhereAmI.Helpers.SubscribeGeofences(manager);
+            manager.OnEnteredGeofence += GeofenceManager_OnEnteredGeofence;
+            manager.OnExitedGeofence += GeofenceManager_OnExitedGeofence;
+
+            IGeolocator mocklocator = new MockGeolocator();
+            mocklocator.PositionChanged += Locator_PositionChanged;
+
+            foreach (var position in debuggingAppState.LastFivePositions)
+            {
+                ((MockGeolocator)mocklocator).FeedPosition(position);
+
+                Thread.Sleep(1000);
+            }
+        }
+
+        static Position latestPosition { get; set; }
+        static TollboothGeofence lastEnteredTollBooth { get; set; } = null;
+        static RouteGeofence currentRoute { get; set; } = null;
+        static FixedSizeQueue<Position> lastFivePositions { get; set; } = new FixedSizeQueue<Position>(5);
+
+        private static void Locator_PositionChanged(object sender, PositionEventArgs e)
+        {
+            try
+            {
+                var position = e.Position;
+
+                latestPosition = position;
+
+                try
+                {
+                    lastFivePositions.Enqueue(latestPosition);
+                }
+                catch { }
+
+                //UpdateUIWithNewPosition(position);
+                manager.UpdateGeofences(position);
+            }
+            catch (Exception ex)
+            {
+                string msg = $"Excepted On Locator Position Changed {Environment.NewLine}{ex}";
+                AddMessageToUI(msg);
+            }
+        }
+
+        private static void GeofenceManager_OnEnteredGeofence(object sender, GeofenceEnteredEventArgs e)
+        {
+            //await MainPage.DisplayAlert("Exited Geofence!", e.Geofence.Name, "Ok");
+            //mainStack.Children.Add(new Label
+            //{
+            //    HorizontalTextAlignment = TextAlignment.Start,
+            //    HorizontalOptions = LayoutOptions.StartAndExpand,
+            //    LineBreakMode = LineBreakMode.WordWrap,
+            //    Text = $"Entered geofence {e.Geofence.Name} at {e.DateTimeEntered}"
+            //});
+            try
+            {
+                if (e.Geofence is TollboothGeofence)
+                {
+                    lastEnteredTollBooth = e.Geofence as TollboothGeofence;
+                }
+                else if (e.Geofence is RouteGeofence)
+                {
+                    // handle crossing over routes
+                    if (currentRoute == null)
+                    {
+                        if (lastEnteredTollBooth != null)
+                        {
+                            currentRoute = e.Geofence as RouteGeofence;
+                            AddMessageToUI($"Got on {currentRoute.Name} via the {lastEnteredTollBooth.Name} exit.");
+                        }
+                    }
+                }
+                else
+                {
+                    AddMessageToUI($"Entered geofence {e.Geofence.Name} at {e.DateTimeEntered}");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                string msg = $"Excepted On Entered Geofence {Environment.NewLine}{ex}";
+                AddMessageToUI(msg);
+            }
+        }
+
+        private static void AddMessageToUI(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+
+        private static void GeofenceManager_OnExitedGeofence(object sender, GeofenceExitedEventArgs e)
+        {
+            //await MainPage.DisplayAlert("Entered Geofence!", e.Geofence.Name, "Ok");
+            //mainStack.Children.Add(new Label
+            //{
+            //    HorizontalTextAlignment = TextAlignment.Start,
+            //    HorizontalOptions = LayoutOptions.StartAndExpand,
+            //    LineBreakMode = LineBreakMode.WordWrap,
+            //    Text = $"Exited geofence {e.Geofence.Name} at {e.DateTimeExited}"
+            //});
+            try
+            {
+                if (e.Geofence is TollboothGeofence)
+                {
+                    //lastEnteredTollBooth = e.Geofence as TollboothGeofence;
+                    lastEnteredTollBooth = null;
+                }
+                else if (e.Geofence is RouteGeofence)
+                {
+                    if (e.Geofence == currentRoute)
+                    {
+                        if (e.Geofence == null)
+                        {
+                            AddMessageToUI($"Got off {currentRoute.Name} via unspecified exit at ({latestPosition.Latitude}, {latestPosition.Longitude})");
+                        }
+                        else
+                        {
+                            AddMessageToUI($"Got off {currentRoute.Name} via the {lastEnteredTollBooth.Name} exit.");
+                        }
+                        currentRoute = null;
+                        lastEnteredTollBooth = null;
+                    }
+                }
+                else
+                {
+                    AddMessageToUI($"Exited geofence {e.Geofence.Name} at {e.DateTimeExited}");
+                }
+            }
+            catch (Exception ex)
+            {
+
+                string msg = $"Excepted On Exited Geofence {Environment.NewLine}{ex}{Environment.NewLine}";
+                AddMessageToUI(msg);
+            }
+        }
+    }
+}
---
+++ WhereAmI/WhereAmI.Mock/MockGeolocator.cs
@@ -0,0 +1,130 @@
+﻿using Plugin.Geolocator.Abstractions;
+using System;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace WhereAmI.Mock
+{
+    public class MockGeolocator : IGeolocator
+    {
+        bool mAllowsBackgroundUpdates;
+        public bool AllowsBackgroundUpdates
+        {
+            get
+            {
+                return mAllowsBackgroundUpdates;
+            }
+
+            set
+            {
+                mAllowsBackgroundUpdates = value;
+            }
+        }
+
+        double mDesiredAccuracy;
+        public double DesiredAccuracy
+        {
+            get
+            {
+                return mDesiredAccuracy;
+            }
+
+            set
+            {
+                mDesiredAccuracy = value;
+            }
+        }
+
+        bool mIsGeolocationAvailable;
+        public bool IsGeolocationAvailable
+        {
+            get
+            {
+                return mIsGeolocationAvailable;
+            }
+        }
+
+        bool mIsGeolocationEnabled;
+        public bool IsGeolocationEnabled
+        {
+            get
+            {
+                return mIsGeolocationEnabled;
+            }
+        }
+
+        bool mIsListening { get; set; } = false;
+
+        public bool IsListening
+        {
+            get
+            {
+                return mIsListening;
+            }
+        }
+
+        bool mPausesLocationUpdatesAutomatically;
+        public bool PausesLocationUpdatesAutomatically
+        {
+            get
+            {
+                return mPausesLocationUpdatesAutomatically;
+            }
+
+            set
+            {
+                mPausesLocationUpdatesAutomatically = value;
+            }
+        }
+
+        bool mSupportsHeading;
+        public bool SupportsHeading
+        {
+            get
+            {
+                return mSupportsHeading;
+            }
+        }
+
+        public event EventHandler<PositionEventArgs> PositionChanged;
+        public event EventHandler<PositionErrorEventArgs> PositionError;
+
+        Position lastPosition;
+        
+        public Task<Position> GetPositionAsync(int timeoutMilliseconds = -1, CancellationToken? token = default(CancellationToken?), bool includeHeading = false)
+        {
+            return Task.Run(() => lastPosition);
+        }
+
+        int mMinTime { get; set; } = 0;
+        double mMinDistance { get; set; } = 0;
+
+        public Task<bool> StartListeningAsync(int minTime, double minDistance, bool includeHeading = false)
+        {
+            mIsListening = true;
+            mMinTime = minTime;
+            mMinDistance = minDistance;
+            return Task.Run(() => mIsListening);
+        }
+
+        public Task<bool> StopListeningAsync()
+        {
+            mIsListening = false;
+            return Task.Run(() => mIsListening);
+        }
+
+        public void FeedPosition(Position position)
+        {
+            lastPosition = position;
+            
+            if (mIsListening)
+            {
+                // TODO something with pythagoreas for min distance
+                if ((position.Timestamp - lastPosition.Timestamp).TotalMinutes >= mMinTime)
+                {
+                    PositionChanged?.Invoke(this, new PositionEventArgs(position));
+                }
+            }
+        }
+    }
+}
